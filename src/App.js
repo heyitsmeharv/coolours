0d27aeb7200aa235fcfc242b440fa0176899e81d
Adam Harvey <heyitsmeharv@gmail.com>
2020-04-05 11:48:11 +0100

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+import colour from './resources/styles/colours';
+
+const App = () => {
+  return (
+    <div>colour generator</div>
+  );
+}
+
+export default App;
---
+++ src/resources/styles/colours.js
@@ -0,0 +1,11 @@
+const colours = {
+  primary: '#f05a28',
+  secondary: '#fff0bc',
+  accent: '#fefcdb',
+  complement: '#f7931e',
+  border: '#00204a',
+  black: '#000000',
+  white: '#ffffff',
+}
+
+export default colours
