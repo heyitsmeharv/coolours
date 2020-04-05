21df29fd0e7ba4c58690db379c8e1ec2206e37f2
Adam Harvey <heyitsmeharv@gmail.com>
2020-04-05 12:29:46 +0100

    colour presets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import colour from './resources/styles/colours';
+// import colour from './resources/styles/colours';
 
 const App = () => {
   return (
