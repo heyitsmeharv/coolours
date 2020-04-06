2e34af5d778a050f137789a2d8c399dfc90a4c43
Adam Harvey <heyitsmeharv@gmail.com>
2020-04-06 09:22:30 +0100

    playing with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
 
+import Tile from './components/Tile/Tile';
+// import TopBar from './components/TopBar/TopBar';
+
+import './style.scss';
 // import colour from './resources/styles/colours';
+
 
 const App = () => {
   return (
-    <div>colour generator</div>
+    <>
+      {/* <TopBar /> */}
+      <div className="container-fluid">
+        <div className="d-flex justify-content-left">
+          <div className="d-flex justify-content-center">
+            <h1>Colour Generator</h1>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center">
+            <Tile />
+          </div>
+          <div className="align-self-center">
+            <button type="button" className="btn btn-primary">Generate</button>
+          </div>
+          <div className="d-flex justify-content-center">
+            <Tile />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const TopBar = () => {
+  return (
+    <section className="TopBar">
+      <h1>Colour Generator</h1>
+    </section>
+  )
+}
+
+export default TopBar 
